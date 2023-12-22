b4b47fb9fe5e4bd4ea96943810cd9d4bf30a7867
Reza shakeri <rzashakeri@gmail.com>
2023-12-22 04:34:21 +0330

    ✨ feat: Add base_conversation_handler to main.py and reformat code

--- main.py
+++ main.py
@@ -2,21 +2,31 @@ from telegram.ext import Application
 from telegram.ext import CommandHandler
 from telegram.ext import filters
 from telegram.ext import MessageHandler
+from telegram.ext import PicklePersistence
 
 from commands.maintenance import maintenance
 from configurations import settings
 from configurations.settings import IS_MAINTENANCE
+from core.handlers import base_conversation_handler
 from utils import logger
 
 if __name__ == "__main__":
     logger.init_logger(f"logs/{settings.NAME}.log")
-    application = (Application.builder().token(
-        settings.TOKEN).read_timeout(50).write_timeout(
-            50).get_updates_read_timeout(50).persistence(persistence).build())
+    persistence = PicklePersistence(filepath="conversation states")
+    application = (
+        Application
+        .builder()
+        .token(settings.TOKEN)
+        .read_timeout(50)
+        .write_timeout(50)
+        .get_updates_read_timeout(50)
+        .persistence(persistence)
+        .build()
+    )
     if IS_MAINTENANCE:
         application.add_handler(CommandHandler("start", maintenance))
         application.add_handler(
             MessageHandler(filters.TEXT & ~filters.COMMAND, maintenance))
     else:
-        """YOUR COMMANDS IS HERE WHEN BOT IS NOT MAINTENANCE"""
+        application.add_handler(base_conversation_handler())
     application.run_polling()
