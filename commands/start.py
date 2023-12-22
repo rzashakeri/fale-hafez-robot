3cebc2accf46e1eacb7cffd5dad4d39769830d11
Reza shakeri <rzashakeri@gmail.com>
2023-12-22 04:33:23 +0330

    ♻️ refactor: improve start command

--- commands/start.py
+++ commands/start.py
@@ -3,6 +3,10 @@ from logging import getLogger
 from telegram import Update
 from telegram.constants import ChatAction
 from telegram.ext import ContextTypes
+
+from constants.messages import WELCOME_MESSAGE
+from constants.states import HOME_STATE
+from core.keyboards import base_keyboard
 from utils.decorators import send_action
 
 # Init logger
@@ -15,5 +19,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> str:
     """Select an action: Adding parent/child or show data."""
     # pylint: disable=unused-argument
     await update.message.reply_text(
-        text="Hi, Welcome",
+        text=WELCOME_MESSAGE,
+        reply_markup=base_keyboard
     )
+    return HOME_STATE
